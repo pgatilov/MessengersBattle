ab83fe5d85c811a97fdf1ec63aa78e0a15a46b1f
Pavel Gatilov <pgatilov@outlook.com>
2013-12-15 02:23:44 +0700

    Added test console app to check .net low-trust case

---
+++ LowTrustTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace LowTrustTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new Listener();
+            listener.Subscribe();
+
+            Messenger.Default.Send(new object());
+
+            Console.ReadLine();
+        }
+
+        private class Listener
+        {
+            public void Subscribe()
+            {
+                Messenger.Default.Register<object>(this, x => Console.WriteLine("Ping"));
+            }
+        }
+    }
+}
